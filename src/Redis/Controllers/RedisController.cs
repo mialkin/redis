2ce98f15a4bbc8a05b3a28c2fc7cce5f3a1f5258
Мялкин Алексей Николаевич <a.myalkin@tinkoff.ru>
2023-02-20 21:41:34 +0300

    Use RedisClient

--- src/Redis/Controllers/RedisController.cs
+++ src/Redis/Controllers/RedisController.cs
@@ -1,13 +1,31 @@
+using System;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Redis.Redis;
 
 namespace Redis.Controllers;
 
 [Route("redis")]
 public class RedisController : ControllerBase
 {
-    [HttpGet("hello")]
-    public string SayHello()
+    private readonly IRedisClient _redisClient;
+
+    public RedisController(IRedisClient redisClient) => _redisClient = redisClient;
+
+    [HttpGet("add-random-key-value")]
+    public async Task<IActionResult> AddRandomKeyValue()
     {
-        return "Hello";
+        var randomKey = "random_key:" + Guid.NewGuid();
+        var value = "value_" + Guid.NewGuid();
+
+        await _redisClient.SetAsync(randomKey, value, TimeSpan.FromHours(12));
+        return Ok(randomKey);
+    }
+
+    [HttpGet("get-value-by-key")]
+    public async Task<IActionResult> GetValueByKey(string key)
+    {
+        var value = await _redisClient.GetAsync<string>(key);
+        return Ok(value);
     }
 }
